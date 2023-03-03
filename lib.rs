43f159b54a90f452b8a4d17bcad532eb59382bbc
Ganesh oli <ganesh@thaiofficefurniture.com>
2023-03-03 09:00:53 +0545

    wip e2e_test decoding

--- lib.rs
+++ lib.rs
@@ -19,6 +19,12 @@ mod event_test {
         value: bool,
     }
 
+    #[ink(event)]
+    pub struct MyEvent2 {
+        #[ink(topic)]
+        flip_value: i32,
+    }
+
     impl EventTest {
         /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
@@ -40,7 +46,7 @@ mod event_test {
         #[ink(message)]
         pub fn flip(&mut self) {
             self.value = !self.value;
-            self.env().emit_event(MyEvent { value: self.value });
+            self.env().emit_event(MyEvent2 { flip_value: 100 })
         }
 
         /// Simply returns the current value of our `bool`.
@@ -50,10 +56,14 @@ mod event_test {
         }
     }
 
+    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
+    /// module and test functions are marked with a `#[test]` attribute.
+    /// The below code is technically just normal Rust code.
     // #[cfg(test)]
     // mod tests {
     //     use ink::env::test::EmittedEvent;
 
+    //     /// Imports all the definitions from the outer scope so we can use them here.
     //     use super::*;
 
     //     type Event = <EventTest as ::ink::reflect::ContractEventBase>::Type;
@@ -67,12 +77,14 @@ mod event_test {
     //             .collect()
     //     }
 
+    //     /// We test if the default constructor does its job.
     //     #[ink::test]
     //     fn default_works() {
     //         let event_test = EventTest::default();
     //         assert_eq!(event_test.get(), false);
     //     }
 
+    //     /// We test a simple use case of our contract.
     //     #[ink::test]
     //     fn it_works() {
     //         let mut event_test = EventTest::new(false);
@@ -89,7 +101,102 @@ mod event_test {
     //         assert_eq!(event_test.get(), true);
     //     }
     // }
-}
 
-#[cfg(all(test, feature = "e2e-tests"))]
-mod e2e_tests;
+    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
+    ///
+    /// When running these you need to make sure that you:
+    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
+    /// - Are running a Substrate node which contains `pallet-contracts` in the background
+    #[cfg(all(test, feature = "e2e-tests"))]
+    mod e2e_tests {
+        /// Imports all the definitions from the outer scope so we can use them here.
+        use super::*;
+
+        /// A helper function used for calling contract messages.
+        use ink_e2e::build_message;
+
+        /// The End-to-End test `Result` type.
+        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+        /// We test that we can upload and instantiate the contract using its default constructor.
+        #[ink_e2e::test]
+        async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+            // Given
+            let constructor = EventTestRef::default();
+
+            // When
+            let contract_account_id = client
+                .instantiate("event_test", &ink_e2e::alice(), constructor, 0, None)
+                .await
+                .expect("instantiate failed")
+                .account_id;
+
+            // Then
+            let get = build_message::<EventTestRef>(contract_account_id.clone())
+                .call(|event_test| event_test.get());
+            let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
+            assert!(matches!(get_result.return_value(), false));
+
+            Ok(())
+        }
+
+        /// We test that we can read and write a value from the on-chain contract contract.
+        #[ink_e2e::test]
+        async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+            // Given
+            let constructor = EventTestRef::new(false);
+            let contract_account_id = client
+                .instantiate("event_test", &ink_e2e::alice(), constructor, 0, None)
+                .await
+                .expect("instantiate failed")
+                .account_id;
+
+            let get = build_message::<EventTestRef>(contract_account_id.clone())
+                .call(|event_test| event_test.get());
+            let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
+            assert!(matches!(get_result.return_value(), false));
+
+            // When
+            let flip = build_message::<EventTestRef>(contract_account_id.clone())
+                .call(|event_test| event_test.flip());
+            let _flip_result = client
+                .call(&ink_e2e::alice(), flip, 0, None)
+                .await
+                .expect("flip failed");
+
+            // Filter the events
+            let contract_emitted_event = _flip_result
+                .events
+                .iter()
+                .find(|event| {
+                    event
+                        .as_ref()
+                        .expect("Expect Event")
+                        .event_metadata()
+                        .event()
+                        == "ContractEmitted"
+                })
+                .expect("Expect ContractEmitted event")
+                .unwrap();
+
+            // Decode to the expected event type
+            let event = contract_emitted_event.field_bytes();
+            let decode_event =
+                <MyEvent2 as scale::Decode>::decode(&mut &event[1..]).expect("invalid data");
+
+            // Destructor
+            let MyEvent2 { flip_value } = decode_event;
+
+            // Assert with expected value
+            assert_eq!(flip_value, 100);
+
+            // Then
+            let get = build_message::<EventTestRef>(contract_account_id.clone())
+                .call(|event_test| event_test.get());
+            let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
+            assert!(matches!(get_result.return_value(), true));
+
+            Ok(())
+        }
+    }
+}
